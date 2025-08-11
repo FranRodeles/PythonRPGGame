b3c638d053a9111aeb1c83465d726e0df9aac419
Franco <f.gentile@alumno.um.edu.ar>
2025-08-11 19:35:06 -0300

    Title and Option text done, print_options in progress

--- Menu/menu.py
+++ Menu/menu.py
@@ -12,17 +12,30 @@ class Menu():
         self.choice_made = False
         self.console = console
         self.game_window = game_window
-        
-    def print_menu(self,text,options):
+ 
+        #Printea El titulo
+    def print_menu(self):
+        self.console.print(Panel(self.text,width=64),justify="center")
+        #Printea Las opciones
+    def print_option(self):
+
+
+
+        #Texto del Juego.El titulo-una breve descripcion y las opciones a elegir
+    def text_menu(self):
         text = ([red] "Divine Light" [/red]"\n"
                 [yellow]"This Game is Awesome" [/yellow])           #Titulo con una breve descripcion
 
         options = { 01 : "Nuevo Juego",
-                    02 : "Cargar Partida",
-                    03 : "Salir del Juego"}
-        
-        self.text = text
-        self.options = options
+                    02 : "Cargar Partida",    
+                    03 : "Salir del Juego"}   #Opciones
 
         os.system("clear")
 
+        #Crear el Menu
+        starting_menu = Menu(text,options,console,game_window)
+
+        #Mostrar el menu
+        os.system("clear")
+        starting_menu.print_menu()
+        starting_menu.print_option()
