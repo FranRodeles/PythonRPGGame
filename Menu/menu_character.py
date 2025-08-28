a46164811fe32aec540e6a12d8356844072f8b35
Franco <f.gentile@alumno.um.edu.ar>
2025-08-28 19:53:21 -0300

    Main_character fixed

--- Menu/menu_character.py
+++ Menu/menu_character.py
@@ -29,7 +29,7 @@ class main_character(Menu):
 
         panels = []
         for char in self.options:
-            content = f"[bold]{char['name']}[/bold]\n\n{char['role']}\n\n{char['desc']}\n\n[bold red]ATK[/bold red]: {char['atk']}  [bold blue]MAGE[/bold blue]: {char['mag']}  [bold green]ACCURACY[/bold green]: {char['acu']}"
+            content = f"[bold]{char['name']}[/bold]\n\n{char['role']}\n\n{char['desc']}\n\n[bold red]ATK[/bold red]:{char['atk']}  [bold blue]MAGE[/bold blue]:{char['mag']}  [bold white]ACCURACY[/bold white]:{char['acu']}  [bold green]DEFENSE[/bold green]: {char['def']}"
 
             if char["id"] == self.current_option:
                 panel = Panel(content, title="▶ Seleccionado ◀", border_style="bold yellow")
@@ -68,9 +68,9 @@ class main_character(Menu):
 #------LOGICA------    
 text= "[bold magenta]⚔️ Selección de Personajes ⚔️[/bold magenta]"
 character = [
-    {"id" : 0,"name": "Lobo","role": "Paladin","desc":"El personaje mas basado 😎", "atk" : 9, "acu" : 9, "mag" : 0 },
-    {"id" : 1,"name": "Gandalf","role": "Wizard","desc" : "pi piri piri pi PI PI PIPI", "atk" : 3, "acu" : 4, "mag" : 11 },
-    {"id" : 2,"name": "Robin Hood", "role": "Archer","desc":"SI SACO LA GUN", "atk" : 5, "acu" : 9, "mag" : 4 }
+    {"id" : 0,"name": "Lobo","role": "Paladin","desc":"El personaje mas basado 😎", "atk" : 7, "acu" : 7, "mag" : 0 ,"def":5},
+    {"id" : 1,"name": "Gandalf","role": "Wizard","desc" : "pi piri piri pi PI PI PIPI", "atk" : 3, "acu" : 4, "mag" : 10,"def":2 },
+    {"id" : 2,"name": "Robin Hood", "role": "Archer","desc":"SI SACO LA GUN", "atk" : 3, "acu" : 7, "mag" : 4,"def":4 }
 ]
 
 if __name__ == "__main__":
