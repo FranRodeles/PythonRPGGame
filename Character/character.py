e0925f47a944b61f5a4e08faa8b6541aefb73838
Franco <f.gentile@alumno.um.edu.ar>
2025-09-02 15:10:58 -0300

    Character modificado

--- Character/character.py
+++ Character/character.py
@@ -1,19 +1,29 @@
 
 class Character():
-    def __init__(self,name,type,atk,mage,accuracy,level,experience):
+    def __init__(self,name,vida,type,atk,mage,accuracy,defense,level):
         self.name = name
+        self.vida = vida
         self.type = type
         self.atk = atk
         self.mage = mage
         self.accuracy = accuracy
+        self.defense = defense
         self.level = level
-        self.experience = experience
-        self.puntos_atributos = puntos_atributos
+
+
+
+
+
+
+
+
+
 
     def subir_level(self):
         self.experience = 0
         self.level = self.level + 1
         self.puntos_atributos = self.puntos_atributos + 3
+
         
 
 
